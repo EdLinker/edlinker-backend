11acc510d11198b8e5195b2f5401938f52337837
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-03-06 15:30:56 +0200

    add students table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_221015) do
+ActiveRecord::Schema.define(version: 2021_03_06_132101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 2021_03_05_221015) do
     t.string "name"
     t.string "tuition_form"
     t.integer "course_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "patronymic"
+    t.string "phone_number"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
