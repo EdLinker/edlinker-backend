a5a7acb0885609a897386aac3b59cf46332e585c
Anton <antonkonovalov167@gmail.com>
2021-03-17 22:06:13 +0200

    change field 'status' in posts to integer & add enum statuses in post

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   has_many   :messages
   belongs_to :student
+
+  enum status: [:new, :in_progress, :done]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_164609) do
+ActiveRecord::Schema.define(version: 2021_03_17_175618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,9 +94,9 @@ ActiveRecord::Schema.define(version: 2021_03_15_164609) do
     t.integer "student_id"
     t.string "description"
     t.string "link"
-    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0, null: false
   end
 
   create_table "teachers", force: :cascade do |t|
