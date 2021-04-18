40a90cd177655b560762dbbf3dfee8078c3a402d
Anton <antonkonovalov167@gmail.com>
2021-04-18 20:39:33 +0300

    delete url_temp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_181465) do
+ActiveRecord::Schema.define(version: 2021_04_18_173712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,6 @@ ActiveRecord::Schema.define(version: 2021_04_16_181465) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.jsonb "url", default: {}
-    t.string "url_temp", default: ["test", "test2"], array: true
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
