19e461c0ba9228b95593ac7d998067d8de116ddc
Anton <antonkonovalov167@gmail.com>
2021-04-22 23:04:14 +0300

    fix auditoriums

--- app/api/edlinker/auditoriums.rb
+++ app/api/edlinker/auditoriums.rb
@@ -12,7 +12,7 @@ class Edlinker::Auditoriums < Grape::API
             group_id: group&.id,
             subject_name: group&.subject&.name,
             course_number: group&.course_number,
-            student_count: group&.users.count,
+            student_count: group&.users&.count,
             group_leader_id: group&.group_leader_id
         }
       end
