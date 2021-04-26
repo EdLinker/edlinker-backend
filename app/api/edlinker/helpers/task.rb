0f3df1b0ab05b9a0c043e1f9393cd162b1e8e1db
Anton <antonkonovalov167@gmail.com>
2021-04-26 21:10:06 +0300

    add pagination

--- app/api/edlinker/helpers/task.rb
+++ app/api/edlinker/helpers/task.rb
@@ -3,6 +3,7 @@ module Edlinker::Helpers::Task
   params :task_params do
     requires :title, type: String
     requires :subject_id, type: Integer
+    requires :page, type: Integer
     optional :auditorium_id, type: Integer
     optional :description, type: String
     optional :status, type: Integer
