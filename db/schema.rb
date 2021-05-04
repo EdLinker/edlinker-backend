25af7eff26c4d01356beec24119653e91e11d247
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-05-04 15:13:25 +0300

    add telegram data field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_183442) do
+ActiveRecord::Schema.define(version: 2021_04_29_190940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_183442) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "avatar"
+    t.json "telegram_data"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
