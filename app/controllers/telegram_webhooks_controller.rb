abf1c1ff4fb6c04708808b30236575a766335f07
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-05-04 15:20:54 +0300

    add phone number request

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,12 +1,31 @@
-class TelegramWebhooksController <  Telegram::Bot::UpdatesController
+class TelegramWebhooksController < Telegram::Bot::UpdatesController
+  include Telegram::Bot::UpdatesController::MessageContext
   Telegram.bots_config = {
-      default: ENV["TELEGRAM_TOKEN"]
+    default: ENV['TELEGRAM_TOKEN']
   }
+  @current_user = User.find_by(phone_number: @contact)
   def start!(*)
-    respond_with :message, text: t('telegram_webhooks.start.content')
+    phone_number
+    # respond_with :message, text: "#{chat['id']}"
   end
 
   def help!(*)
     respond_with :message, text: t('telegram_webhooks.help.content')
+  end
+
+  def phone_number(*)
+    save_context :phone_number
+    message = update['message']
+    @contact = message.dig('contact', 'phone_number')
+    if @contact.nil?
+      respond_with :message, text: t('telegram_webhooks.phone_number.prompt'), reply_markup: {
+        keyboard: [[text: 'Share phone number', request_contact: true]],
+        resize_keyboard: true,
+        one_time_keyboard: true,
+        selective: true
+      }
+    else
+      respond_with :message, text: t('telegram_webhooks.phone_number.saved')
+    end
   end
 end
