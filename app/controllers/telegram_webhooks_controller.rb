2c64fb0d19bccda8e5049efd8d170f0699a14f1f
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-05-05 23:41:51 +0300

    add telegram data update

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -3,20 +3,26 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
   Telegram.bots_config = {
     default: ENV['TELEGRAM_TOKEN']
   }
-  @current_user = User.find_by(phone_number: @contact)
+  
   def start!(*)
     phone_number
-    # respond_with :message, text: "#{chat['id']}"
+
   end
 
   def help!(*)
     respond_with :message, text: t('telegram_webhooks.help.content')
   end
 
+  def mail!(*)
+
+  end
+
   def phone_number(*)
     save_context :phone_number
     message = update['message']
+    
     @contact = message.dig('contact', 'phone_number')
+    puts @contact
     if @contact.nil?
       respond_with :message, text: t('telegram_webhooks.phone_number.prompt'), reply_markup: {
         keyboard: [[text: 'Share phone number', request_contact: true]],
@@ -25,6 +31,12 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
         selective: true
       }
     else
+      @current_user = User.find_by(phone_number: @contact)
+      puts @current_user
+      @current_user.update_column(:telegram_data, from)
+
+
+      respond_with :message, text: @current_user.telegram_data
       respond_with :message, text: t('telegram_webhooks.phone_number.saved')
     end
   end
