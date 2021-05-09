d8e76e49bb15275c2344b584b69fcb496e6af92c
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-05-10 00:28:00 +0300

    send messages to students via telegram bot

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ApplicationRecord
+  after_create :send_task
   belongs_to :task
-  belongs_to :user
+  belongs_to :author, polymorphic: true
+
+  private
+  def send_task
+    text = "#{body} #{url.first if url}"
+    Telegram.bot.send_message(chat_id: task.user.telegram_data['chat_id'], text: text)
+  end
 end
