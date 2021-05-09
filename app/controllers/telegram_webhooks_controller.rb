0e4b03d902a94fb59cb1a408f056afd65c14dd98
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-05-10 00:29:18 +0300

    add status & subject inline keyboard

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -3,26 +3,71 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
   Telegram.bots_config = {
     default: ENV['TELEGRAM_TOKEN']
   }
-  
+
   def start!(*)
     phone_number
-
   end
 
   def help!(*)
     respond_with :message, text: t('telegram_webhooks.help.content')
   end
 
-  def mail!(*)
+  def main_inline_menu!
+    @current_user = User.where("telegram_data->>'user_id' = ?", from['id'].to_s).first
+    subjects = []
+    subject_buttons = []
 
+    @current_user.tasks.each do |task|
+      subjects << task.subject.name
+    end
+
+    subjects.each do |subject|
+      subject_buttons << {text: subject, callback_data: subject}
+    end
+
+    respond_with :message, text: 'Choose your subject', reply_markup: {
+      inline_keyboard: [
+        subject_buttons
+      ]
+    }
   end
 
-  def phone_number(*)
+  def change_last_task_status!
+    respond_with :message, text: 'Choose your progress', reply_markup: {
+      inline_keyboard: [
+        [
+          {text: 'ToDo', callback_data: 'todo'},
+          {text: 'In progress', callback_data: 'in_progress'},
+          {text: 'Done', callback_data: 'done'},
+          {text: 'Problems', callback_data: 'problems'}
+        ]
+      ]
+    }
+  end
+
+  def callback_query(data)
+    @current_user = User.where("telegram_data->>'user_id' = ?", from['id'].to_s).first
+    task = @current_user.tasks[-1]
+    case data
+    when 'todo'
+      task.update_column(:status, 0)
+      p task
+      answer_callback_query 'Status changed to ToDo'
+    when 'in_progress'
+      task.update_column(:status, 1)
+      p task
+      answer_callback_query 'Status changed to In Progress'
+    when 'done'
+      task.update_column(:status, 2)
+      p task
+      answer_callback_query 'Status changed to Done'
+    end
+  end
+
+  def phone_number
     save_context :phone_number
     message = update['message']
-    
     @contact = message.dig('contact', 'phone_number')
-    puts @contact
     if @contact.nil?
       respond_with :message, text: t('telegram_webhooks.phone_number.prompt'), reply_markup: {
         keyboard: [[text: 'Share phone number', request_contact: true]],
@@ -31,13 +76,15 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
         selective: true
       }
     else
-      @current_user = User.find_by(phone_number: @contact)
-      puts @current_user
-      @current_user.update_column(:telegram_data, from)
-
-
-      respond_with :message, text: @current_user.telegram_data
+      update_tg_data
       respond_with :message, text: t('telegram_webhooks.phone_number.saved')
     end
+  end
+
+  def update_tg_data
+    @current_user = User.find_by(phone_number: @contact)
+    puts "This is #{@current_user.first_name}"
+    tg_data = {user_id: from['id'], chat_id: chat['id']}
+    @current_user.update_column(:telegram_data, tg_data)
   end
 end
