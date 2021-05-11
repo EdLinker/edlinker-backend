c286fcec90adb330e5640dfd47117f5491bdbbb4
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-05-11 18:19:43 +0300

    bot updates

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,12 @@
 class Message < ApplicationRecord
   after_create :send_task
   belongs_to :task
-  # belongs_to :author, polymorphic: true
+  belongs_to :author, polymorphic: true
 
   #private
   def send_task
 
-    text = "#{body} #{url.first if url}"
+    text = "#{body} \n#{url.first if url}"
     Telegram.bot.send_message(chat_id: task.user.telegram_data['chat_id'], text: text)
   end
 end
